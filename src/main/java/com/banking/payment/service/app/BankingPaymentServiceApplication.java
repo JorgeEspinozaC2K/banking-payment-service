cc3cce57cc311959932175af2ffce980d424fde3
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-04-20 18:37:13 -0500

    Initial commit to payment service

---
+++ src/main/java/com/banking/payment/service/app/BankingPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.banking.payment.service.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/petition/service/app/BankingPetitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.petition.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingPetitionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
