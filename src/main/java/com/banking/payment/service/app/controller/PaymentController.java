1305c9b7bf28301d69991f9a9e93c8562e0ef94f
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-04-21 17:39:12 -0500

    updating

--- src/main/java/com/banking/payment/service/app/controller/PaymentController.java
+++ src/main/java/com/banking/payment/service/app/controller/PaymentController.java
@@ -16,7 +16,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("api/v1/payments")
+@RequestMapping("/payment")
 public class PaymentController {
 	
 	@Autowired
